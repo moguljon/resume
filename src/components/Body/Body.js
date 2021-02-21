98aec22ca082abbe40ed4813dc06ae325083be4f
unknown <jonathanaguilar@moguljon.com>
2021-02-21 02:03:34 -0500

    added

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Body = ({headertwo, paragraph, link1, link2}) => {
     return(
-        <div>
-             <h2 className='person__headertwo'>{headertwo}</h2>
+        <div className='ani'>
+             <h1 className='person__big'>{headertwo}</h1>
              <p className='person__paragraph'>{paragraph}</p>
-             <Link to='/work' className='btn btn-primary'>{link1}</Link>
-             <Link to='/resume'className='btn btn-ghost'>{link2}</Link>
+             <Link to='/work' className='btn btn-primary btn-animated'>{link1}</Link>
+             <Link to='/resume'className='btn btn-ghost btn-animated'>{link2}</Link>
         </div>
     )
 }
