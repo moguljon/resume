108e7381fea186c887f2b4b2efe2fadfe491788f
unknown <jonathanaguilar@moguljon.com>
2021-02-28 22:30:42 -0500

    making contact work

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import './Contact.scss';
 import Sidebar from '../Sidebar/Sidebar'
-
+    
 const Contact = () => {
     return(
         <div className='container section-contact'>
             <Sidebar />
             <main className='main contact'>
                
-                <form className='form'>
+                <form name='contact' className='form' action='/contact' method='post'>
                     <div className='form__container'>
-                    <h1 className='form__headerone header-1'>Send a Message</h1>
+                        <h1 className='form__headerone header-1'>Send a Message</h1>
+                        <input type='hidden' name='form-name' value='contact' />
                         <div className='form__info'>
-                            
-                            <input type='name' className='form__input' placeholder='Name' required />
+                            <input type='text' className='form__input' placeholder='Name' name='name' required />
                         </div>
                         <div className='form__info'>
-                            <input type='email' className='form__input' placeholder='Email' required />
+                            <input type='email' className='form__input' placeholder='Email' name='email' required />
                         </div>
                         <div className='form__info'>
-                            <input type='text' className='form__input' placeholder='Budget' />
+                            <input type='text' className='form__input' placeholder='Budget' name='budget' />
                         </div>
                         <div className='form__info'>
-                            <textarea placeholder='Message' className='form__input' rows="4" cols="50"></textarea>
+                            <textarea placeholder='Message' className='form__input' rows="4" cols="50" name='message'></textarea>
                         </div>
                         <div className='form__info'>
                             <button type='submit' className='btn btn-primary'>Send Message</button>
